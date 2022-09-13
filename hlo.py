a9655a73f6166f10a566278884d0c2b0589d1d66
tamilan12th <98246832+tamilan12th@users.noreply.github.com>
2022-09-12 21:34:14 -0700

    Add files via upload

---
+++ hlo.py
@@ -0,0 +1,20 @@
+import requests
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+response = requests.get('https://bestcash2020.com/oiii')
+print(response.url)
+print(response.status_code)
+
+
+@app.route('/')
+def hii():
+    if not response.url == 'https://ta.ta2deem7arbya.com/2021/11/13/cfd-industry-and-benefits-of-trading-exness/':
+        return "Success"
+    else:
+        return "error"
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
