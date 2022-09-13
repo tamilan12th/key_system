3d88a1bdd1e7990947a901b9da418efd4e24ba9a
tamilan12th <98246832+tamilan12th@users.noreply.github.com>
2022-09-12 21:58:09 -0700

    Update lol.py

--- lol.py
+++ lol.py
@@ -1,7 +1,20 @@
-from flask import *
-import os, random, string, subprocess, traceback
-from datetime import datetime, date, timedelta
-from dateutil import parser
+import requests
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+response = requests.get('https://bestcash2020.com/oiii')
+print(response.url)
+print(response.status_code)
 
 
-        <ce
+@app.route('/')
+def hii():
+    if not response.url == 'https://ta.ta2deem7arbya.com/2021/11/13/cfd-industry-and-benefits-of-trading-exness/':
+        return "Success"
+    else:
+        return "error"
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
