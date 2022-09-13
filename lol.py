1339b0bfa6aea1b891c2a2945c0832b459aaa9ef
tamilan12th <98246832+tamilan12th@users.noreply.github.com>
2022-09-13 17:29:18 +0530

    Update lol.py

--- lol.py
+++ lol.py
@@ -1,5 +1,5 @@
 import requests
-from flask import Flask, render_template
+from flask import Flask
 
 app = Flask(__name__)
 
@@ -15,6 +15,3 @@ def hii():
     else:
         return "error"
 
-
-if __name__ == '__main__':
-    app.run(debug=True)
