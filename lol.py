12f26f0fe3f3fa269ea610c1bf866c1c2091edbd
tamilan12th <98246832+tamilan12th@users.noreply.github.com>
2022-09-13 17:31:34 +0530

    Update lol.py

--- lol.py
+++ lol.py
@@ -1,17 +1,8 @@
 import requests
-from flask import Flask
 
-app = Flask(__name__)
 
 response = requests.get('https://bestcash2020.com/oiii')
 print(response.url)
 print(response.status_code)
 
-
-@app.route('/')
-def hii():
-    if not response.url == 'https://ta.ta2deem7arbya.com/2021/11/13/cfd-industry-and-benefits-of-trading-exness/':
-        return "Success"
-    else:
-        return "error"
 
